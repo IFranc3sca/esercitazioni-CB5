9f7bd533666605715a19589adaa84a50fa4b5f7f
IFranc3sca <leonardif05@gmail.com>
2022-11-04 12:26:04 +0100

    esercitazione 03-11-22

---
+++ 03-11-22/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = "rgb(16, 15, 15)";
+  document.body.style.color = "white";
+});
