81662a0cd5ef9a2ed0c61bb252af1c4fc7b98599
IFranc3sca <leonardif05@gmail.com>
2022-11-09 19:07:33 +0100

    esercitazione 09-11-22

--- 09-11-22/script.js
+++ 09-11-22/script.js
@@ -1,27 +1,10 @@
-/*const colors = {
-  fire: "#FDDFDF",
-  grass: "#DEFDE0",
-  electric: "#FCF7DE",
-  water: "#DEF3FD",
-  ground: "#f4e7da",
-  rock: "#d5d5d4",
-  fairy: "#fceaff",
-  poison: "#98d7a5",
-  bug: "#f8d5a3",
-  dragon: "#97b3e6",
-  psychic: "#eaeda1",
-  flying: "#F5F5F5",
-  fighting: "#E6E0D4",
-  normal: "#F5F5F5",
-};*/
 const bodyEl = document.querySelector("body");
 const container = document.querySelector(".cards");
-//const col = Object.keys(colors);
 
 const createCard = (imgUrl, id, name, type, parent) => {
   const cardEl = document.createElement("div");
   cardEl.className = "card";
-  //cardEl.className = `bg-${type}`;  //prova per il bg color
+  cardEl.classList.add(type);
 
   const idEl = document.createElement("span");
   idEl.textContent = id;
@@ -35,12 +18,7 @@ const createCard = (imgUrl, id, name, type, parent) => {
   nameEl.textContent = name;
 
   const typeEl = document.createElement("h2");
-  typeEl.textContent = type;
-  /*for (let i = 0; i < col.length; i++) {     //prova non funzionante per il bg color
-    if (col[i] === type) {
-    //   cardEl.classList.add(`${type}`);
-    }
-  }*/
+  typeEl.textContent = `Type: ${type}`;
   cardEl.append(imgEl, idEl, nameEl, typeEl);
   parent.appendChild(cardEl);
 };
@@ -54,7 +32,7 @@ const createCard = (imgUrl, id, name, type, parent) => {
          res.sprites.front_default,
          `#${res.id}`,
         res.name[0].toUpperCase() + res.name.slice(1),
-         `Type: ${res.types[0].type.name}`,
+         res.types[0].type.name,
          container
        )
      );
@@ -81,7 +59,7 @@ Promise.all(request).then((res) =>
       pokemon.sprites.other.dream_world.front_default,
       `#${pokemon.id.toString().padStart(3, "0")}`,
       pokemon.name[0].toUpperCase() + pokemon.name.slice(1),
-      `Type: ${pokemon.types[0].type.name}`,
+      pokemon.types[0].type.name,
       container
     );
   })
